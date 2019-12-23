425de91a5581b266086d73d0c257258200470f31
M Saif Ullah <chsaif369@gmail.com>
2019-12-24 00:04:13 +0500

    Filed Injection With Dagger2

--- app/src/main/java/com/saifi369/daggerkotlinexample/MainActivity.kt
+++ app/src/main/java/com/saifi369/daggerkotlinexample/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.saifi369.daggerkotlinexample.R.layout.activity_main
 import com.saifi369.daggerkotlinexample.di.DaggerCarInjector
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var hondaCar: Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main)
 
-        val hondaCar = DaggerCarInjector.create().getCar()
+        DaggerCarInjector.create().injectFields(this)
 
         hondaCar.drive()
     }
--- app/src/main/java/com/saifi369/daggerkotlinexample/di/CarInjector.kt
+++ app/src/main/java/com/saifi369/daggerkotlinexample/di/CarInjector.kt
@@ -1,11 +1,14 @@
 package com.saifi369.daggerkotlinexample.di
 
 import com.saifi369.daggerkotlinexample.Car
+import com.saifi369.daggerkotlinexample.MainActivity
 import dagger.Component
 
 @Component
 interface CarInjector {
 
     fun getCar(): Car
+
+    fun injectFields(mainActivity: MainActivity)
 
 }
